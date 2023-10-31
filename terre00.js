95b628f0dc7863580733b278f66280c394cbd618
web-orbit12 <goblogger125@gmail.com>
2023-10-31 20:26:04 +0400

    L’alphabet

---
+++ terre00.js
@@ -0,0 +1,7 @@
+/* Créez un programme qui affiche l’alphabet en lettres minuscules suivi d’un retour à la ligne. */
+
+let alpha = "abcdefghijklmnopqrstuvwxyz";
+
+for (let i = 0; i < alpha.length; i++) {
+        console.log(alpha[i]);
+}
